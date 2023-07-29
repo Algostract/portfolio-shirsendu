8bc07296b962a3ea8b95d3870b68d8d4ba8db2e5
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-07-29 10:18:24 +0530

    minor fixes

--- components/Section/Hackathon.vue
+++ components/Section/Hackathon.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="hackathon" class="h-screen">
-    <h4 class="mx-auto w-fit text-lg lg:text-xl">All Hackathons</h4>
+  <section id="hackathon" class="relative flex flex-col gap-8 max-h-screen">
+    <h4 class="mx-auto mb-8 w-fit text-lg lg:text-xl">All Hackathons</h4>
     <div class="flex flex-col gap-4 my-8">
     </div>
   </section>
