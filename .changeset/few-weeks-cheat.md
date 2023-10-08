356b4b8f184eda897150ed326e7926b576172645
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-10-08 11:20:57 +0530

    fix: hackathon card light mode and alignment

---
+++ .changeset/few-weeks-cheat.md
@@ -0,0 +1,5 @@
+---
+"portfolio": patch
+---
+
+fix: hackathon card light mode
--- components/Section/Hackathon.vue
+++ components/Section/Hackathon.vue
@@ -6,7 +6,7 @@ const { data: hackathons, pending, error } = await useFetch("/api/hackathon")
 <template>
   <section id="hackathon" class="relative flex flex-col gap-8">
     <h4 class="mx-auto mb-8 w-fit text-lg">All Hackathons</h4>
-    <div class="relative -left-12 flex flex-wrap justify-center gap-5 py-4 px-12 w-screen overflow-hidden">
+    <div class="relative  flex flex-wrap justify-center gap-5 py-4 overflow-hidden">
       <HackathonCard v-for="{ name, url, project, title, startedAt } in hackathons" :name="name" :url="url"
         :project="project" :title="title" :startedAt="startedAt" />
     </div>
