567efbdf93ff6f9911e2d244428f7e6eb1a1bccc
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-12-02 23:09:36 +0530

    refactor: added file parsing on /experience

--- app.vue
+++ app.vue
@@ -20,6 +20,56 @@ useHead({
     lang: 'en'
   }
 })
+
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://shirsendu-bairagi.dev',
+    },
+  ],
+})
+
+useSeoMeta({
+  ogType: "profile",
+  title: "Shirsendu's Portfolio",
+  ogTitle: "Shirsendu's Portfolio",
+  description: "I am a Web Developer and an AI Enthusiast, this is my developer's portfolio showcasing my skills, projects, and expertise in a visually appealing manner",
+  ogDescription: "I am a Web Developer and an AI Enthusiast, this is my developer's portfolio showcasing my skills, projects, and expertise in a visually appealing manner",
+  ogImage: "https://shirsendu-bairagi.dev/previews/landing.jpg",
+  ogImageWidth: 1280,
+  ogImageHeight: 640,
+  ogUrl: 'https://shirsendu-bairagi.dev',
+  fbAppId: 966242223397117,
+  twitterCard: "summary_large_image",
+})
+
+useSchemaOrg([
+  definePerson({
+    name: "Shirsendu Bairagi",
+    description: "I am a Web Developer and an AI Enthusiast",
+    image: "https://shirsendu-bairagi.dev/logo.png",
+    sameAs: [
+      "https://github.com/shba007",
+      "https://www.linkedin.com/in/shirsendu-bairagi-28b4b1213",
+      "https://www.youtube.com/@shirsendu_bairagi",
+      "https://twitter.com/shirsendu_2001",
+      "https://www.instagram.com/shirsendu_bairagi",
+      "https://www.facebook.com/shirsendu.bairagi.1"
+    ],
+  }),
+  defineWebSite({
+    name: "Shirsendu's Portfolio",
+    description: `I am a Web Developer and an AI Enthusiast, this is my 
+    developer's portfolio showcasing my skills, projects,
+    and expertise in a visually appealing manner`,
+  }),
+  defineWebPage({
+    datePublished: new Date(2023, 5, 14).toISOString(),
+    dateModified: new Date(2023, 5, 20).toISOString(),
+    author: "Shirsendu Bairagi",
+  }),
+])
 </script>
 
 <template>
--- server/api/hackathon.ts
+++ server/api/hackathon.ts
@@ -1,20 +1,22 @@
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
 import yaml from "yaml";
 
-export default defineEventHandler((event) => {
+export interface Hackathon {
+  name: string;
+  url: string;
+  project: string;
+  title: string | null;
+  startedAt: string;
+}
+
+export default defineEventHandler<Hackathon[]>((event) => {
   const config = useRuntimeConfig()
 
   try {
     const filePath = path.join(process.cwd(), config.private.rootDir, 'hackathons.yml')
     const fileContents = fs.readFileSync(filePath, "utf8");
-    const hackathons: {
-      name: string;
-      url: string;
-      project: string;
-      title: string | null;
-      startedAt: string;
-    }[] = yaml.parse(fileContents);
+    const hackathons: Hackathon[] = yaml.parse(fileContents);
 
     return hackathons
   } catch (error: any) {
