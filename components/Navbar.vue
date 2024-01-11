d2dd6463a74b1c7372181956a101998d159bee72
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-01-12 00:29:36 +0530

    chore: pkgs updated

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,21 +9,11 @@ function onNavigate(section: string) {
 <template>
   <nav class="flex items-center justify-between">
     <ul class="hidden md:flex gap-7">
-      <li>
-        <NuxtLink to="#project" @click="onNavigate('project')">Projects</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="#hackathon" @click="onNavigate('hackathon')">Hackathons</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="#experience" @click="onNavigate('experience')">Experiences</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="#certificate" @click="onNavigate('certificate')">Certificates</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="#about" @click="onNavigate('about')">About Me</NuxtLink>
-      </li>
+      <li><NuxtLink to="#project" @click="onNavigate('project')">Projects</NuxtLink></li>
+      <li><NuxtLink to="#hackathon" @click="onNavigate('hackathon')">Hackathons</NuxtLink></li>
+      <li><NuxtLink to="#experience" @click="onNavigate('experience')">Experiences</NuxtLink></li>
+      <li><NuxtLink to="#certificate" @click="onNavigate('certificate')">Certificates</NuxtLink></li>
+      <li><NuxtLink to="#about" @click="onNavigate('about')">About Me</NuxtLink></li>
     </ul>
     <div class="flex gap-2 items-center ml-auto">
       <ColorModeButton />
