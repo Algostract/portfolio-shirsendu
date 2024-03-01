1199abcff5f16b9d4528f72c92fa7d779beefe58
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-03-01 13:07:22 +0530

    style: fixed timeline colormode

--- components/Section/Experience.vue
+++ components/Section/Experience.vue
@@ -5,12 +5,12 @@ const experiences = computed(() => data.value?.sort((a, b) => new Date(b.date).g
 </script>
 
 <template>
-  <section id="experience" class="relative flex flex-col gap-8 overflow-hidden">
+  <section id="experience" class="relative -left-4 md:left-0 flex flex-col gap-8 w-screen md:w-full overflow-hidden">
     <h4 class="mx-auto w-max text-lg">Work Experiences</h4>
     <div
-      class="relative grid grid-cols-2 grid-flow-row justify-center before:content-[''] before:w-[4.5px] before:h-[calc(100%-108px)] before:absolute before:bg-dark-600 before:left-1/2 before:top-1/2 before:-translate-y-1/2">
-      <TimelineBranch v-for="{ company, date }, index in experiences" :key="company" :side="index % 2 ? 'left' : 'right'"
-        :style="{ gridRowStart: index + 1 }"
+      class="relative grid grid-cols-2 grid-flow-row justify-center mb-8 before:content-[''] before:w-[4.5px] before:h-[calc(100%-108px)] before:absolute before:bg-light-500 before:dark:bg-dark-600 before:left-1/2 before:top-1/2 before:-translate-y-1/2">
+      <TimelineBranch v-for="{ company, date }, index in experiences" :key="company"
+        :side="index % 2 ? 'left' : 'right'" :style="{ gridRowStart: index + 1 }"
         :class="index % 2 ? 'col-start-1 justify-self-end translate-x-[8px]' : 'col-start-2 justify-self-start -translate-x-[4px]'"
         :company="company" :date="date" />
     </div>
--- components/TimelineBranch.vue
+++ components/TimelineBranch.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import brancHead from "~/assets/images/branch-head.svg?raw";
-import branchTail from "~/assets/images/branch-tail.svg?raw";
+import branchHead from "~/assets/images/branch-head.svg?raw";
 
 const { side = 'left' } = defineProps<{
   side?: 'left' | 'right',
@@ -11,15 +10,15 @@ const { side = 'left' } = defineProps<{
 function capitalize(str: string) {
   return str.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ")
 }
-
 </script>
 
 <template>
   <div class="relative w-[102%]" :class="side === 'left' ? 'left-2' : 'right-2'">
     <div class="relative flex justify-center items-center" :class="{ 'rotate-180': side !== 'left' }">
-      <span v-html="brancHead" />
-      <div class="flex-1 w-full h-1 bg-dark-500" />
-      <span v-html="branchTail" />
+      <span v-html="branchHead" class="fill-light-500 dark:fill-dark-500" />
+      <div class="flex-1 w-full h-1 bg-light-500 dark:bg-dark-500" />
+      <span
+        class="relative w-7 aspect-square rounded-full bg-light-500 dark:bg-dark-500 after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:w-4 after:aspect-square after:rounded-full after:bg-light-400 after:dark:bg-dark-400" />
     </div>
     <NuxtLink :aria-label="company" :to="`https://www.linkedin.com/company/${company}`" target="_blank">
       <img :src="`/companies/${company}.jpg`" :alt="company" loading="lazy"
@@ -29,7 +28,7 @@ function capitalize(str: string) {
     <!-- -->
     <time :datetime="date" class="absolute top-6 sm:top-4 text-sm sm:text-lg"
       :class="side === 'left' ? 'right-4 sm:right-1/2 -translate-x-1/2 sm:translate-x-1/2' : 'left-4 sm:left-1/2 translate-x-1/2 sm:-translate-x-1/2'">{{
-        date }}</time>
+    date }}</time>
     <span class="absolute" :class="side === 'left' ? 'left-0' : 'right-0'">
       {{ capitalize(company.split("-").join(" ")) }}
     </span>
