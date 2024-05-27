0db53263ddac7bbd6ba51d056e1c8be3bb730990
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-05-27 23:58:50 +0530

    seo: h tag issue fixed, images more optimised

---
+++ .changeset/eight-swans-turn.md
@@ -0,0 +1,5 @@
+---
+"portfolio-shirsendu": patch
+---
+
+seo: h tag issue fixed, images more optimised
--- components/Section/Experience.vue
+++ components/Section/Experience.vue
@@ -6,7 +6,7 @@ const experiences = computed(() => data.value?.sort((a, b) => new Date(b.date).g
 
 <template>
   <section id="experience" class="relative -left-4 md:left-0 flex flex-col gap-8 w-screen md:w-full overflow-hidden">
-    <h4 class="mx-auto w-max text-lg">Work Experiences</h4>
+    <h3 class="mx-auto w-max text-lg">Work Experiences</h3>
     <div
       class="relative grid grid-cols-2 grid-flow-row justify-center mb-8 before:content-[''] before:w-[4.5px] before:h-[calc(100%-108px)] before:absolute before:bg-light-500 before:dark:bg-dark-600 before:left-1/2 before:top-1/2 before:-translate-y-1/2">
       <TimelineBranch v-for="{ company, date }, index in experiences" :key="company"
